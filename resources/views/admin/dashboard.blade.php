bd678991de64e65603ee81877a30b607cf5d6d1f
JoseSimoes <josesimoesk10@gmail.com>
2025-03-07 20:19:39 -0300

    Refatora viwes para saber o tipo do usuario logado

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') . \Auth::guard('admin')->user()->name }}
+            {{ __('Dashboard') . " do Admin: ". \Auth::guard('admin')->user()->name  }}
         </h2>
     </x-slot>
 
