7bcd0dca8bf2db971ffa5ca5c4bb9e1ae2ae5056
JoseSimoes <josesimoesk10@gmail.com>
2025-03-08 21:44:32 -0300

    Adiciona botões de navegação para gerenciamento de usuários, admins e produtos no dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,8 +8,22 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="p-6 text-gray-900 flex gap-20">
+                    <div>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <a href="{{ route('userIndex') }}">Ir para Tabela de Gerenciamento de Usuarios</a>
+                        </button>
+                    </div>
+                    <div>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <a href="{{ route('adminIndex') }}">Ir para Tabela de Gerenciamento de Admins</a>
+                        </button>
+                    </div>
+                    <div>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <a href="{{ route('productIndex') }}">Ir para Tabela de Gerenciamento de Produtos</a>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
