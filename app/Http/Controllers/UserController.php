91df3a7c4b1442ae74b170a61fb22e03a9a93462
JoseSimoes <josesimoesk10@gmail.com>
2025-03-10 04:55:33 -0300

    Adiciona métodos para criação e exclusão de perfis de usuário no UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -32,4 +33,33 @@ class UserController extends Controller
          return redirect()->route('editProfile', $user->id);
 
     }
+
+    public function viewCreateProfile()
+    {
+        return view('admin.createProfile');
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'date_birth' => $request->date_birth,
+            'password' => Hash::make($request->password),
+            'address' => $request->address,
+            'number' => $request->number,
+            'cpf' => $request->cpf,
+            'balance' => $request->balance,
+            'image' => $request->image,
+            'admin_id' => logged_admin()->id,
+        ]);
+        return redirect()->route('createProfile');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->back();
+    }
+
 }
