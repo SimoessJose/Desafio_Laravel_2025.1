b22c9dc8d87401f47d427104deb1ceb3e23f99ac
JoseSimoes <josesimoesk10@gmail.com>
2025-03-11 04:58:12 -0300

    Adiciona página de erro de pagamento

---
+++ resources/views/user/paymentError.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+
+    <body class="bg-gray-100 flex items-center justify-center min-h-screen">
+        <div class="bg-white p-6 rounded-lg shadow-lg text-center">
+            <img src="https://cdni.iconscout.com/illustration/free/thumb/free-payment-error-illustration-download-in-svg-png-gif-file-formats--no-transaction-made-unsuccessful-failed-digital-business-pack-illustrations-6369553.png"
+                alt="Payment Error" class="mx-auto mb-4">
+            <h1 class="text-2xl font-bold mb-2">Payment Error</h1>
+            <p class="text-gray-700">There was an error with your payment. Please try again.</p>
+        </div>
+    </body>
+</x-app-layout>
