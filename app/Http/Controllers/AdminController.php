a536bb0f9383b98fdb3cd4eb4719e591014ab737
JoseSimoes <josesimoesk10@gmail.com>
2025-03-11 09:22:06 -0300

    adiciona funcionalidades para gerenciar perfis de administrador, incluindo criação, edição e exclusão

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
@@ -20,5 +22,71 @@ class AdminController extends Controller
 
 
         return view('admin.adminTable', compact('admins'));
+    }
+
+    public function edit(Admin $admin)
+    {
+        return view('admin.updateAdminProfile', compact('admin'));
+    }
+
+    public function view(Admin $admin)
+    {
+        return view('admin.viewAdminProfile', compact('admin'));
+    }
+
+    public function update(Admin $admin, Request $request)
+    {
+        $data = $request->except('image');
+
+
+        $admin->update($data);
+
+        if ($request->hasFile('image')) {
+
+            if ($admin->image) {
+                Storage::disk('public')->delete($admin->image);
+            }
+
+            $imagePath = $request->file('image')->store('profiles', 'public');
+
+            $admin->image = $imagePath;
+            $admin->save();
+        }
+
+        return redirect()->route('editAdminProfile', $admin->id);
+    }
+
+    public function viewCreateProfile()
+    {
+        return view('admin.createAdminProfile');
+    }
+
+    public function store(Request $request) 
+{
+    if ($request->hasFile('image')) {
+        $imagePath = $request->file('image')->store('profiles', 'public');
+    } else {
+        $imagePath = null;
+    }
+    
+    Admin::create([
+        'name'       => $request->name,
+        'email'      => $request->email,
+        'date_birth' => $request->date_birth,
+        'password'   => Hash::make($request->password),
+        'address'    => $request->address,
+        'number'     => $request->number,
+        'cpf'        => $request->cpf,
+        'image'      => $imagePath,
+        'admin_id'   => logged_admin()->id,
+    ]);
+    
+    return redirect()->route('createProfile');
+}
+
+    public function destroy(Admin $admin)
+    {
+        $admin->delete();
+        return redirect()->back();
     }
 }
