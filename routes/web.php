7487ae7858311d973eceddad6977ac2f894e6605
JoseSimoes <josesimoesk10@gmail.com>
2025-03-12 05:31:31 -0300

    refatora rotas de administração e adiciona funcionalidades de retirada para usuários

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,9 @@ Route::get('/dashboard', function () {
     return view('user.landingPage');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
-});
+
+    Route::get('/dashboard', function () { return view('admin.dashboard');  })->name('admin.dashboard');
+
 
 
 Route::middleware('auth')->group(function () {
@@ -30,8 +28,21 @@ Route::middleware('auth')->group(function () {
 });
 
 
+Route::middleware('auth_admin')->group(function(){
+    Route::get('/adminsTable', [AdminController::class, 'index'])->name('adminIndex');
+    Route::get('/viewAdminProfile/{admin}', [AdminController::class, 'view'])->name('viewAdminProfile');
+    Route::get('/createAdminProfile', [AdminController::class, 'viewCreateProfile'])->name('createAdminProfile');
+    Route::delete('/delete-admin/{admin}', [AdminController::class, 'destroy'])->name('deleteAdmin');
+    Route::post('/storeAdmin', [AdminController::class, 'store'])->name('storeAdminProfile');
+    Route::get('/admin/updateAdminProfile/{admin}', [AdminController::class, 'edit'])->name('editAdminProfile');
+    Route::put('/updateAdminProfile/{admin}', [AdminController::class, 'update'])->name('updateAdminProfile');
+    Route::get('/createProfile', [UserController::class, 'viewCreateProfile'])->name('createProfile');
+    Route::post('/store', [UserController::class, 'store'])->name('storeProfile');
+    
+});
+
+
 Route::get('/usersTable', [UserController::class, 'index'])->name('userIndex');
-Route::get('/adminsTable', [AdminController::class, 'index'])->name('adminIndex');
 Route::get('/produtsTable', [ProductController::class, 'index'])->name('productIndex');
 Route::get('/landingPage', [ProductController::class, 'search'])->name('index');
 Route::get('/search', [ProductController::class, 'search'])->name('productsSearch');
@@ -39,23 +50,11 @@ Route::get('/product/{product}', [ProductController::class, 'show'])->name('user
 Route::get('/updateProfile/{user}', [UserController::class, 'edit'])->name('editProfile');
 Route::get('/viewProfile/{user}', [UserController::class, 'view'])->name('viewProfile');
 Route::put('/updateProfile/{user}', [UserController::class, 'update'])->name('updateUser');
-Route::get('/createProfile', [UserController::class, 'viewCreateProfile'])->name('createProfile');
-Route::post('/store', [UserController::class, 'store'])->name('storeProfile');
 Route::delete('/delete/{user}', [UserController::class, 'destroy'])->name('deleteUser');
 Route::get('/payment-error', function () {
     return view('user.paymentError');
 })->name('paymentError');
 Route::post('/checkout', [PagSeguroController::class, 'createCheckout'])->name('checkout');
-
-
-
-Route::get('/viewAdminProfile/{admin}', [AdminController::class, 'view'])->name('viewAdminProfile');
-Route::get('/createAdminProfile', [AdminController::class, 'viewCreateProfile'])->name('createAdminProfile');
-Route::delete('/delete-admin/{admin}', [AdminController::class, 'destroy'])->name('deleteAdmin');
-Route::post('/storeAdmin', [AdminController::class, 'store'])->name('storeAdminProfile');
-Route::get('/admin/updateAdminProfile/{admin}', [AdminController::class, 'edit'])->name('editAdminProfile');
-Route::put('/updateAdminProfile/{admin}', [AdminController::class, 'update'])->name('updateAdminProfile');
-
 
 Route::get('/createProductProfile', [ProductController::class, 'viewCreateProfile'])->name('createProductProfile');
 Route::delete('/delete-product/{product}', [ProductController::class, 'destroy'])->name('deleteProduct');
@@ -63,5 +62,7 @@ Route::post('/storeProduct', [ProductController::class, 'store'])->name('storePr
 Route::get('/admin/updateProductProfile/{product}', [ProductController::class, 'edit'])->name('editProductProfile');
 Route::put('/updateProductProfile/{product}', [ProductController::class, 'update'])->name('updateProductProfile');
 
+Route::get('/withdraw/{user}', [UserController::class, 'withdrawView'])->name('withdrawView');
+Route::put('/withdraw/{user}', [UserController::class, 'withdraw'])->name('withdraw');
 
 require __DIR__ . '/auth.php';
