ca36fc1c61f8dc6f0467e41ccf9d933693127477
JoseSimoes <josesimoesk10@gmail.com>
2025-03-12 05:30:26 -0300

    adiciona botões de navegação para gerenciamento de admins e área de saque no dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') . " do Admin: ". \Auth::guard('admin')->user()->name  }}
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
@@ -9,21 +9,31 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 flex gap-20">
+                    @if(is_admin())
+                        <div>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <a href="{{ route('adminIndex') }}">Ir para Tabela de Gerenciamento de Admins</a>
+                            </button>
+                        </div>
+                    @endif
                     <div>
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             <a href="{{ route('userIndex') }}">Ir para Tabela de Gerenciamento de Usuarios</a>
                         </button>
                     </div>
-                    <div>
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            <a href="{{ route('adminIndex') }}">Ir para Tabela de Gerenciamento de Admins</a>
-                        </button>
-                    </div>
+
                     <div>
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             <a href="{{ route('productIndex') }}">Ir para Tabela de Gerenciamento de Produtos</a>
                         </button>
                     </div>
+                    @if(is_user())
+                        <div>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <a href="{{ route('withdrawView', logged_user()->id) }}">Ir para Area de Saque</a>
+                            </button>
+                        </div>
+                    @endif    
                 </div>
             </div>
         </div>
