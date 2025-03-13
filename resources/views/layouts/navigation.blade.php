54599bc90cc25cde2ef5ece06e9ba5a67e8e4c2e
JoseSimoes <josesimoesk10@gmail.com>
2025-03-13 07:44:06 -0300

    adiciona link para a página de contato na navegação

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,6 +24,14 @@
                     </x-nav-link>
                     
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    
+                    <x-nav-link :href="route('contact.index')" :active="request()->routeIs('index')">
+                        {{ __('Contact') }}
+                    </x-nav-link>
+                    
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
