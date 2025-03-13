d5a074bc3307388a0d1f623e2a5727bedbb836f5
JoseSimoes <josesimoesk10@gmail.com>
2025-03-13 07:44:56 -0300

    adiciona rotas para gerenciamento de contato e relatórios de vendas

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Middleware\auth_user;
+use App\Http\Middleware\auth_admin;
+use App\Http\Middleware\Auth_User_AdminMiddleware;
 use Illuminate\Support\Facades\App;
 use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\Transaction;
@@ -21,7 +25,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
-    Route::get('/dashboard', function () { return view('admin.dashboard');  })->name('admin.dashboard');
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
 
 
 
@@ -32,7 +38,7 @@ Route::middleware('auth')->group(function () {
 });
 
 
-Route::middleware('auth_admin')->group(function(){
+Route::middleware(auth_admin::class)->group(function () {
     Route::get('/adminsTable', [AdminController::class, 'index'])->name('adminIndex');
     Route::get('/viewAdminProfile/{admin}', [AdminController::class, 'view'])->name('viewAdminProfile');
     Route::get('/createAdminProfile', [AdminController::class, 'viewCreateProfile'])->name('createAdminProfile');
@@ -42,44 +48,75 @@ Route::middleware('auth_admin')->group(function(){
     Route::put('/updateAdminProfile/{admin}', [AdminController::class, 'update'])->name('updateAdminProfile');
     Route::get('/createProfile', [UserController::class, 'viewCreateProfile'])->name('createProfile');
     Route::post('/storeProfile', [UserController::class, 'store'])->name('storeProfile');
-    
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 });
 
 
-Route::get('/usersTable', [UserController::class, 'index'])->name('userIndex');
-Route::get('/produtsTable', [ProductController::class, 'index'])->name('productIndex');
-Route::get('/landingPage', [ProductController::class, 'search'])->name('index');
-Route::get('/search', [ProductController::class, 'search'])->name('productsSearch');
-Route::get('/product/{product}', [ProductController::class, 'show'])->name('user.productView');
-Route::get('/updateProfile/{user}', [UserController::class, 'edit'])->name('editProfile');
-Route::get('/viewProfile/{user}', [UserController::class, 'view'])->name('viewProfile');
-Route::put('/updateProfile/{user}', [UserController::class, 'update'])->name('updateUser');
-Route::delete('/delete/{user}', [UserController::class, 'destroy'])->name('deleteUser');
-Route::get('/payment-error', function () {
-    return view('user.paymentError');
-})->name('paymentError');
-Route::post('/checkout', [PagSeguroController::class, 'createCheckout'])->name('checkout');
+Route::middleware(auth_user::class)->group(function () {
+    Route::get('/payment-error', function () {
+        return view('user.paymentError');
+    })->name('paymentError');
+    Route::post('/checkout', [PagSeguroController::class, 'createCheckout'])->name('checkout');
+    Route::get('/createProductProfile', [ProductController::class, 'viewCreateProfile'])->name('createProductProfile');
+    Route::post('/storeProduct', [ProductController::class, 'store'])->name('storeProduct');
+    Route::get('/withdraw/{user}', [UserController::class, 'withdrawView'])->name('withdrawView');
+    Route::put('/withdraw/{user}', [UserController::class, 'withdraw'])->name('withdraw');
+    Route::get('/purchase', [TransactionController::class, 'purchase'])->name('purchase');
+    Route::get('/relatorioCompras', function () {
 
-Route::get('/createProductProfile', [ProductController::class, 'viewCreateProfile'])->name('createProductProfile');
-Route::delete('/delete-product/{product}', [ProductController::class, 'destroy'])->name('deleteProduct');
-Route::post('/storeProduct', [ProductController::class, 'store'])->name('storeProduct');
-Route::get('/admin/updateProductProfile/{product}', [ProductController::class, 'edit'])->name('editProductProfile');
-Route::put('/updateProductProfile/{product}', [ProductController::class, 'update'])->name('updateProductProfile');
+        $products = Transaction::where('buyer_id', logged_user()->id)->with('product')->get();
 
-Route::get('/withdraw/{user}', [UserController::class, 'withdrawView'])->name('withdrawView');
-Route::put('/withdraw/{user}', [UserController::class, 'withdraw'])->name('withdraw');
+        $pdf = Pdf::loadView('user.purchase-pdf', compact('products'));
+        return $pdf->stream('Relatorio_Compras.pdf');
+    })->name('relatorioCompras');
+});
 
-Route::post('/store', [UserController::class, 'registerUser'])->name('registerProfile');
 
-Route::get('/purchase', [TransactionController::class, 'purchase'])->name('purchase');
+Route::middleware(Auth_User_AdminMiddleware::class)->group(function () {
 
-Route::get('/relatorio', function () {
+    Route::get('/usersTable', [UserController::class, 'index'])->name('userIndex');
+    Route::get('/produtsTable', [ProductController::class, 'index'])->name('productIndex');
+    Route::get('/landingPage', [ProductController::class, 'search'])->name('index');
+    Route::get('/search', [ProductController::class, 'search'])->name('productsSearch');
+    Route::get('/product/{product}', [ProductController::class, 'show'])->name('user.productView');
+    Route::get('/updateProfile/{user}', [UserController::class, 'edit'])->name('editProfile');
+    Route::get('/viewProfile/{user}', [UserController::class, 'view'])->name('viewProfile');
+    Route::put('/updateProfile/{user}', [UserController::class, 'update'])->name('updateUser');
+    Route::delete('/delete/{user}', [UserController::class, 'destroy'])->name('deleteUser');
+    Route::put('/updateProductProfile/{product}', [ProductController::class, 'update'])->name('updateProductProfile');
+    Route::get('/admin/updateProductProfile/{product}', [ProductController::class, 'edit'])->name('editProductProfile');
+    Route::delete('/delete-product/{product}', [ProductController::class, 'destroy'])->name('deleteProduct');
+    Route::get('/sales', [TransactionController::class, 'sales'])->name('sales');
+    Route::get('/relatorioVendas', function () {
 
-    $products = Transaction::where('buyer_id', logged_user()->id)->with('product')->get();
+        if (is_admin()) {
+            $sales = Transaction::paginate(10);
+            $pdf = Pdf::loadView('user.sales-pdf', compact('sales'));
+            return $pdf->stream('Relatorio_Vendas.pdf');
+        } else {
 
-    $pdf = Pdf::loadView('user.purchase-pdf', compact('products'));
-    return $pdf->stream('Relatorio_Compras.pdf');
-})->name('relatorio');
+            $sales = Transaction::whereHas('product', function ($query) {
+                $query->where('user_id', logged_user()->id);
+            })->get();
+
+            $pdf = Pdf::loadView('user.sales-pdf', compact('sales'));
+            return $pdf->stream('Relatorio_Vendas.pdf');
+        }
+    })->name('relatorioVendas');
+    Route::post('/store', [UserController::class, 'registerUser'])->name('registerProfile');
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 require __DIR__ . '/auth.php';
