7d49839bb118ae434fc7f56d6cc5ac303e0540e9
JoseSimoes <josesimoesk10@gmail.com>
2025-03-13 07:43:51 -0300

    ajusta tamanho da imagem dos produtos na tabela do painel administrativo

--- resources/views/admin/productTable.blade.php
+++ resources/views/admin/productTable.blade.php
@@ -52,8 +52,8 @@
                                         <tr>
                                             <td class="px-6 py-4 whitespace-nowrap">
                                                 <div class="flex items-center">
-                                                    <div class="flex-shrink-0 h-10 w-10">
-                                                        <img class="h-10 w-10 rounded-full" src="{{ asset('storage/' . $product->image) }}" alt="">
+                                                    <div class="flex-shrink-0">
+                                                        <img class="h-20 w-30" src="{{ asset('storage/' . $product->image) }}" alt="">
                                                     </div>
                                                     <div class="ml-4">
                                                         <div class="text-sm font-medium text-gray-900">
