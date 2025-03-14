a6814e8f8dc7fc92e2f86c50ce0fe1b6471dca65
JoseSimoes <josesimoesk10@gmail.com>
2025-03-14 07:27:33 -0300

    Remove input balance na edicao de Usuario

--- resources/views/admin/updateProfile.blade.php
+++ resources/views/admin/updateProfile.blade.php
@@ -62,13 +62,6 @@
                             class="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
                             value="{{$user->password}}">
                     </div>
-
-                    <div>
-                        <label for="location" class="block text-sm font-medium text-gray-700">Balance</label>
-                        <input type="text" name="balance" id="balance"
-                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
-                            value="{{ $user->balance }}">
-                    </div>
                     <div>
                         <label for="Address" class="block text-sm font-medium text-gray-700">Address</label>
                         <input type="text" name="address" id="address"
