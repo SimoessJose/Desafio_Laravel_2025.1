c0ed3e7b63dcbf4ec6b04faa0dec585eb62c1696
JoseSimoes <josesimoesk10@gmail.com>
2025-03-14 07:24:30 -0300

    atualiza formulário de contato para incluir ID do usuário e desabilita campos de nome e email

--- resources/views/admin/contact.blade.php
+++ resources/views/admin/contact.blade.php
@@ -2,7 +2,7 @@
 
 
     <section class="bg-white dark:bg-gray-900">
-        <form action="{{ route('contact.store') }}" method="POST">
+        <form action="{{ route('contact.store', $user->id) }}" method="POST">
             @csrf
             <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
@@ -11,14 +11,14 @@
                     <div>
                         <label for="name"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                        <input type="text" name="name" id="name"
+                        <input type="text" name="name" id="name" value="{{ logged_admin()->name }}" disabled
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                             required>
                     </div>
                     <div>
                         <label for="email"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                        <input type="email" name="email" id="email"
+                        <input type="email" name="email" id="email" value="{{ $user->email }}" disabled
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                             required>
                     </div>
