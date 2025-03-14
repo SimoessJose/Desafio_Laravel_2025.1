369a4b6ab40e0eab0ee463ba7e8f92d454c5ea50
JoseSimoes <josesimoesk10@gmail.com>
2025-03-14 07:28:36 -0300

    adiciona funcionalidade de upload de imagem de perfil

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,31 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('registerProfile') }}">
+    <form method="POST" enctype="multipart/form-data" action="{{ route('registerProfile') }}">
         @csrf
+
+        <div class="text-center">
+            <div>
+                <img id="profileImage" src="https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.webp" alt="Profile Picture"
+                    class="rounded-full w-32 h-32 mx-auto border-4 border-indigo-800 mb-4 transition-transform duration-300 hover:scale-105 ring ring-gray-300">
+                    <input type="file" name="image" id="image" hidden onchange="previewImage(event)">
+
+                <label for="image" class="inline-flex items-center">
+                    <svg data-slot="icon" class="w-5 h-5 text-blue-700" fill="none" stroke-width="1.5"
+                        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z">
+                        </path>
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z">
+                        </path>
+                    </svg>
+                </label>
+            </div>
+            <button onclick="document.getElementById('image').click()" type="button" 
+                class="bg-indigo-800 text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300 ring ring-gray-300 hover:ring-indigo-300">
+                Change Profile Picture
+            </button>
+        </div>
 
         <!-- Name -->
         <div>
@@ -62,4 +87,21 @@
             </x-primary-button>
         </div>
     </form>
+
+    <script>
+        function previewImage(event) {
+            const input = event.target;
+            const reader = new FileReader();
+    
+            reader.onload = function () {
+                const imgElement = document.getElementById('profileImage');
+                imgElement.src = reader.result; 
+            };
+    
+            if (input.files && input.files[0]) {
+                reader.readAsDataURL(input.files[0]); 
+            }
+        }
+    </script>
+
 </x-guest-layout>
