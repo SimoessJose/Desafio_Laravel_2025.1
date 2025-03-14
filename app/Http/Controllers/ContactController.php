cebeac4b51b6947b54255982654bc48b40522e4e
JoseSimoes <josesimoesk10@gmail.com>
2025-03-14 06:32:37 -0300

    atualiza ContactController para passar usuário logado e ajusta envio de email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,24 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\Contact;
+use App\Models\Admin;
 use App\Models\User;
 class ContactController extends Controller
 {
-    public function index()
-    {
-        return view('admin.contact');
+    public function index(User $user){
+
+        return view('admin.contact', compact('user'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request, User $user){
+        
         $data = [
-            'fromName' => $request->input('name'),
-            'fromEmail' => $request->input('email'),
+            'fromName' => logged_admin()->name,
+            'fromEmail' => logged_admin()->email,
             'subject' => $request->input('subject'),
             'message' => $request->input('message')
 
         ];
 
-        $sent = Mail::to('brunoagjf@gmail.com','Bruno')->send(new Contact($data));
+        $sent = Mail::to($request->input('email'),$request->input('name'))->send(new Contact($data));
 
         return redirect()->back();
     }
